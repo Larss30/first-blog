8b97758d9c7d010853f26e7a5935f3addaab3022
Larss30 <larencearroz@gmail.com>
2023-06-30 14:19:38 +0800

    update Brand component

--- src/Components/brand/Brand.jsx
+++ src/Components/brand/Brand.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './brand.css'
+import { google,  slack,  atlassian, dropbox, shopify } from 'imports.js'
 
 const Brand = () => {
   return (
-    <div>
-      Brand
+    <div className='gpt3_brands'>
+      <img src={ google } alt='google'/>
+      <img src={ slack } alt='slack'/>
+      <img src={ atlassian } alt='atlassian'/>
+      <img src={ dropbox } alt='dropbox'/>
+      <img src={ shopify } alt='shopify'/>
     </div>
   )
 }
