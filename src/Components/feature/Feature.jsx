e6730b2441727c907ee51f4edffb322cc25ad439
Larss30 <larencearroz@gmail.com>
2023-06-30 17:02:30 +0800

    Add What is Chat Chat GPT3 Component

--- src/Components/feature/Feature.jsx
+++ src/Components/feature/Feature.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './feature.css'
 
-const Feature = () => {
+const Feature = ({title, text}) => {
+
   return (
-    <div>
-      Feature
+      <div className='gpt3__features-container__feature'>
+        <div className='gpt3__features-container__feature-title'>
+          <div />
+          <h3>{ title }</h3>
+        </div>
+      <div className='gpt3__features-container_feature-text'>
+      { text }
+      </div>
     </div>
   )
 }
